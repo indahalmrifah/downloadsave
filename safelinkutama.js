46b88cbb244581d9bffb04beaf689989a34a8144
indahalmrifah <31643168+indahalmrifah@users.noreply.github.com>
2019-09-21 19:35:26 +0700

    Update safelinkutama.js

--- safelinkutama.js
+++ safelinkutama.js
@@ -24,8 +24,7 @@ var xxx=null;
 if(x=="1"){xxx="top-mesothelioma-lawyers-secrets-2019.html"}
 if(x=="2"){xxx="what-is-so-fascinating-about-diagnosed.html"}
 if(x=="3"){xxx="mesothelioma-legal-ramifications.html"}
-if(x=="4"){xxx="injury-attorneys-and-law-mesothelioma.html"}
-if(x=="5"){xxx="many-people-have-discovered-that-they.html"}a_to_ve[i].href="https://www.awith.site/2019/09/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="injury-attorneys-and-law-mesothelioma.html"}a_to_ve[i].href="https://www.awith.site/2019/09/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
